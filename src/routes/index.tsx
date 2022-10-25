06d5f540876c07cc62df0bdadbfd127cfbbbfe4c
Raphael Gloria <rapha.gloria@gmail.com>
2022-10-24 23:56:59 -0300

    fix: images not showing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ const MainRoutes = () => {
     <>
       <Routes>
         <Route path="*" element={<>404</>} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
